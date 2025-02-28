92dff1f8dfdc53230ec1b02afceadb9d0be48d2e
rajat <er.rajat153@gmail.com>
2025-03-01 00:42:46 +0530

    Hoisted the project and login logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,49 @@
+import { auth } from "../../utils/firebase"
+import { useEffect } from 'react';
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../../utils/userSlice';
+import { LOGO, USER_DEFAULT_LOGO } from "../../utils/constants";
 
 const Header = () => {
+
+  useEffect(() => {
+   const unsubscribe =  onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const {uid, email, displayName, photoURL} = user;
+        dispatch(addUser({uid : uid, email : email , displayName :displayName, photoURL:photoURL}))
+        navigate('/browse')
+      } else {
+        dispatch(removeUser())
+        navigate('/')
+      }
+    });
+    return () => unsubscribe();
+  }, [])
+
+
+  const user = useSelector(store => store.user)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const handleClick  = () => {
+    signOut(auth).then(() => {
+        navigate('/')
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      }); 
+  }
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
-        <img className = 'w-44' src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
+    <div className='absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10'>
+        <img className = 'w-44' src={LOGO} alt="netflix logo" />
+        { user && <div className="p-2">
+            <p>{user?.displayName}</p>
+         <img className="w-12" src= {USER_DEFAULT_LOGO} alt="" />
+         <button onClick={handleClick}>SignOut</button>
+        </div>}
     </div>
   )
 }
