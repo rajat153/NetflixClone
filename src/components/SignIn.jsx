d13328f7fbb6dc97ad746c490f150d62053a0c9f
rajat <er.rajat153@gmail.com>
2025-03-01 20:39:38 +0530

    add footer in Main page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -6,6 +6,7 @@ import {createUserWithEmailAndPassword, signInWithEmailAndPassword,updateProfile
 import { useDispatch } from 'react-redux';
 import { addUser} from '../../utils/userSlice';
 import { BACKGRND_IMG , USER_DEFAULT_LOGO} from "../../utils/constants";
+import Footer from "./Footer";
 
 
 const SignIn = () => {
@@ -64,20 +65,26 @@ const SignIn = () => {
   return (
     <div>
       <Header/>
-      <div className="absolute">
-        <img className ="w-screen h-screen object-fill" src={BACKGRND_IMG} alt="backround_img" />
+      <div>
+        <img className ="w-screen h-screen" src={BACKGRND_IMG} alt="backround_img" />
       </div>
-      <form action="" onSubmit = {(e)=>e.preventDefault()} className= "w-96 absolute px-12 py-32 my-36 mx-auto right-0 left-0 text-white rounded-lg" style={{backgroundColor: 'rgba(0, 0, 0, 0.85)' }}>
-        <h1 className="font-bold text-3xl py-4">{isSignInForm  ? 'Sign In' : 'Sign Up'}</h1>
-        { !isSignInForm && (<input ref = {name} type = "text" placeholder="Full Name" className="p-2 m-2 border-1 border-gray-200 w-full"/>)}
-        <input ref = {email} type="text" placeholder="Email or mobile number" className="p-2 m-2 border-1 border-gray-200  w-full" />
-        <input ref = {password} type="password" placeholder="Password" className="p-2 m-2 border-1 border-gray-200 w-full"  />
+      <form action="" onSubmit = {(e)=>e.preventDefault()} className= "absolute w-[450px] px-12 py-8 my-24 mx-auto right-0 left-0 top-0 text-white rounded-lg" style={{backgroundColor: 'rgba(0, 0, 0, 0.85)' }}>
+        <h1 className="font-bold text-3xl py-4 mx-2">{isSignInForm  ? 'Sign In' : 'Sign Up'}</h1>
+        { !isSignInForm && (<input ref = {name} type = "text" placeholder="Full Name" className="p-4 rounded-sm  m-2 border-1 border-gray-200 bg-midnight  w-full"/>)}
+        <input ref = {email} type="text" placeholder="Email or mobile number" className="p-4 rounded-sm m-2 border-1 border-gray-200 bg-midnight  w-full" />
+        <input ref = {password} type="password" placeholder="Password" className="p-4 rounded-sm m-2 border-1 border-gray-200 bg-midnight  w-full"  />
         <p className="text-red-500 font-bold text-sm">{errorMsg}</p>
-        <button onClick = {handleClick} className="p-4 my-4 bg-red-700 w-full rounded-sm" >{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
+        <button onClick = {handleClick} className="p-4 my-4 mx-2 bg-red-700 w-full rounded-sm" >{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
         <p className="text-center">OR</p>
         <button className="p-4 m-2 w-full text-center  text-white bg-gray-500 rounded-sm">Use a sign-in code</button>
-        <p className = "py-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ?   <>New to Netflix? <span>Sign up now.</span></> : `Already registered ? Sign In now`}</p>
+        <u className="m-4 block text-center">Forgot password?</u>
+        <input className="absolute h-6 w-6 mx-2 rounded accent-gray-700 bg-black cursor-pointer" type="checkbox" id="remember" name="remember"  />
+        <label className = "mx-10" htmlFor="remember">Remember me</label> 
+        <p className = "mx-2 py-4 cursor-pointer text-gray-400" onClick={toggleSignInForm}>{isSignInForm ? <>New to Netflix?<span className="text-white">Sign up now.</span></> : `Already registered ? Sign In now`}</p>
+        <p className="text-gray-400 mx-2 text-sm">This page is protected by Google reCAPTCHA to ensure you are not a bot.</p>
+        <a href="" className="underline text-blue-600 mx-2 text-sm">Learn more</a>
       </form>
+       <Footer/>
     </div>
   )
 }
