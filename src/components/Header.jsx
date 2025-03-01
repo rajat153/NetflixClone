ca4630d58ffbcd416ca44bf7e905641b05fc1e93
Rajat Chaurasia <68985293+rajat153@users.noreply.github.com>
2025-03-01 23:48:36 +0530

    N footer design (#2)
    * add footer in Main page
    
    * N-Footer-design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
       }); 
   }
   return (
-    <div className='absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10'>
+    <div className='absolute flex justify-between w-screen px-24 py-2 bg-gradient-to-b from-black z-10'>
         <img className = 'w-44' src={LOGO} alt="netflix logo" />
         { user && <div className="p-2">
             <p>{user?.displayName}</p>
