f69eeb70775aba115986ea6bbafa5ae94e5ecf9b
rajat <er.rajat153@gmail.com>
2025-03-30 16:53:11 +0530

    Browse Page design completed

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,17 @@
-import Header from "./Header"
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+
 const Browse = () => {
+  useNowPlayingMovies();
   return (
     <div>
-       <Header/>
+      <Header/>
+      <MainContainer/>
+      <SecondaryContainer/>
     </div>
   )
 }
 
-export default Browse
+export default Browse;
---
+++ src/components/VideoTitle.jsx
@@ -0,0 +1,15 @@
+
+const VideoTitle = ({title, overview}) => {
+  return (
+    <div className='w-screen aspect-video pt-[30%] px-24 absolute bg-gradient-to-r from-black text-white'>
+      <h1 className='text-6xl font-bold'>{title}</h1>
+      <p className='py-2 w-3/4 text-lg'>{overview}</p>
+      <button className='bg-white text-black outline-0 px-10 py-2 rounded-lg text-xl hover:opacity-80'>Play</button>
+      <button className='bg-white text-black outline-0 px-10 py-2 mx-2 rounded-lg text-xl'>More Info</button>
+    </div>
+  )
+}
+
+export default VideoTitle;
+
+
