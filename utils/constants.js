4a877c7f1a67f77c7766644161a5ddc0d857ef35
rajat <er.rajat153@gmail.com>
2025-03-30 16:53:11 +0530

    Browse Page design completed

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,14 @@
 export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const USER_DEFAULT_LOGO = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
 export const BACKGRND_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg";
+export const API_CONSTANTS ={
+    method : "GET",
+    headers : {
+        accept : 'application/json',
+        Authorization: "Bearer 9a98d46d40f53d7a02a68e1c04832bc2",
+    }
+}
+
+//https://thingproxy.freeboard.io/fetch/Your_URL for proxying the API call
+
+export const API_CONSTANT = "9a98d46d40f53d7a02a68e1c04832bc2"
